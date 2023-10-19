9edccfeb516db0174e72a11c894320ca81375fbf
Enzo Magioli <enzomagioli.pro@gmail.com>
2023-10-19 11:52:34 -0300

    fé em deus DJ

--- horarios.py
+++ horarios.py
@@ -7,12 +7,12 @@ linhas = [
     {
         'linha': 'A33',
         'nome': 'Novo Hospital',
-        'h_inicial_tc': ['05:20', '06:40', '06:40'],
-        'h_final_tc': ['22:40', '21:20', '21:20'],
-        'frequencia_tc': ['40min', '80min', '80min'],
-        'h_inicial': ['05:20', '06:00', '06:00'],
-        'h_final': ['22:40', '20:40', '20:40'],
-        'frequencia': ['40min', '80min', '80min'],
+        'h_inicial_tc': [['05:20'], ['06:40'], ['06:40']],
+        'h_final_tc': [['22:40'], ['21:20'], ['21:20']],
+        'frequencia_tc': [['40min'], ['80min'], ['80min']],
+        'h_inicial': [['05:20'], ['06:00'], ['06:00']],
+        'h_final': [['22:40'], ['20:40'], ['20:40']],
+        'frequencia': [['40min'], ['80min'], ['80min']],
         'dias': ['Seg à Sex', 'Sábado', 'Domingo']
     },
     {
@@ -25,6 +25,17 @@ linhas = [
         'h_final': [['21:51'], ['19:15'], ['19:15']],
         'frequencia': [['46min'], ['90min'], ['90min']],
         'dias': ['Seg à Sex', 'Sábado', 'Domingo']
+    },
+    {
+        'linha': 'A73',
+        'nome': 'Engenho da Praia',
+        'h_inicial_tc': [['06:10', '06:48', '07:14', '19:30', '20:50', '21:40'], ['06:30'], ['06:30']],
+        'h_final_tc': [['06:48', '07:14', '19:30', '20:50', '21:40', '22:20'], ['20:00'], ['20:00']],
+        'frequencia_tc': [['38min', '26min', '32min', '40min', '50min', '40min'], ['90min'], ['90min']],
+        'h_inicial': [['05:22', '06:08', '06:26', '19:14', '20:00'], ['07:15'], ['07:15']],
+        'h_final': [['06:08', '06:26', '19:14', '20:00', '21:20'], ['20:45'], ['20:45']],
+        'frequencia': [['46min', '18min', '32min', '46min', '40min'], ['90min'], ['90min']],
+        'dias': ['Seg à Sex', 'Sábado', 'Domingo']
     }
 ]
 
@@ -34,46 +45,50 @@ def print_horarios(h_ini, freq, ciclos):
         print((h_ini + i*freq).strftime('%H:%M'), end='\t')
         if ((i+1) % 4 == 0):
             print()
+    print()
 
-
-linha = linhas[1]
 # Calcular horários durante a semana
 
-print(f'    Linha: {linha["nome"]} ({linha["linha"]})')
-for n in range(0, len(linha['dias'])):
+def seeyouspacecowboy(linha):
+    print(f'    Linha: {linha["nome"]} ({linha["linha"]})')
+    for n in range(0, len(linha['dias'])):
+        print()
+        print('---'*15)
+        print(f'    {linha["dias"][n]}  ')
+        print('---'*15)
+        print(f'Saída {linha["nome"]}:', end='\n\n')
+
+        h_variaveis = len(linha['h_inicial'][n])
+        for f in range(0, h_variaveis):
+            h_ini = dt.strptime(linha['h_inicial'][n][f] + ':00', '%H:%M:%S')
+            h_fim = dt.strptime(linha['h_final'][n][f] + ':00', '%H:%M:%S')
+
+            tempo_circulando = (h_fim - h_ini)
+            freq = timedelta(minutes=float(
+                linha['frequencia'][n][f].replace('min', '')))
+
+            ciclos = int(tempo_circulando.total_seconds() / freq.total_seconds())
+
+            print_horarios(h_ini, freq, ciclos)
+
+            print()
+            print('---'*15,)
+
+        if (linha['frequencia_tc']):
+            h_variaveis = len(linha['h_inicial_tc'][n])
+            # print(f'h_variaveis = {h_variaveis}')
+            # print(f'lista = {linha["h_inicial_tc"]}')
+            # print(f'frequencias = {linha["frequencia_tc"]}')
+            print(f'Saída Terminal Central:', end='\n\n')
+            for f in range(0,h_variaveis):
+                h_ini = dt.strptime(linha['h_inicial_tc'][n][f] + ':00', '%H:%M:%S')
+                h_fim = dt.strptime(linha['h_final_tc'][n][f] + ':00', '%H:%M:%S')
+                tempo_circulando = (h_fim - h_ini)
+                ciclos = int(tempo_circulando.total_seconds() / freq.total_seconds())
+                freq = timedelta(minutes=float(linha['frequencia_tc'][n][f].replace('min', '')))
+                print_horarios(h_ini, freq, ciclos)
     print()
     print('---'*15)
-    print(f'    {linha["dias"][n]}  ')
-    print('---'*15)
-    print(f'Saída {linha["nome"]}:', end='\n\n')
+    print()
 
-    # # checar se h_inicial[n] ou h_inicial_tc[n] é uma lista
-    h_variaveis = len(linha['h_inicial'][n])
-    for f in range(0, h_variaveis):
-        h_ini = dt.strptime(linha['h_inicial'][n][f] + ':00', '%H:%M:%S')
-        h_fim = dt.strptime(linha['h_final'][n][f] + ':00', '%H:%M:%S')
-
-        tempo_circulando = (h_fim - h_ini)
-        freq = timedelta(minutes=float(
-            linha['frequencia'][n][f].replace('min', '')))
-
-        ciclos = int(tempo_circulando.total_seconds() / freq.total_seconds())
-
-        print_horarios(h_ini, freq, ciclos)
-
-        print()
-        print('---'*15,)
-
-    if (linha['frequencia_tc']):
-        h_variaveis = len(linha['h_inicial_tc'][n])
-        # print(f'h_variaveis = {h_variaveis}')
-        # print(f'lista = {linha["h_inicial_tc"]}')
-        # print(f'frequencias = {linha["frequencia_tc"]}')
-        print(f'Saída Terminal Central:', end='\n\n')
-        for f in range(0,h_variaveis):
-            h_ini = dt.strptime(linha['h_inicial_tc'][n][f] + ':00', '%H:%M:%S')
-            h_fim = dt.strptime(linha['h_final_tc'][n][f] + ':00', '%H:%M:%S')
-            tempo_circulando = (h_fim - h_ini)
-            ciclos = int(tempo_circulando.total_seconds() / freq.total_seconds())
-            freq = timedelta(minutes=float(linha['frequencia_tc'][n][f].replace('min', '')))
-            print_horarios(h_ini, freq, ciclos)
+for l in linhas: seeyouspacecowboy(l)
