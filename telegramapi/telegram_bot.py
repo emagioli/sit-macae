7d5782cc99c0e8bd0941a193adce8b86cfb7e040
Enzo Magioli <enzomagioli.pro@gmail.com>
2024-03-25 12:24:00 -0300

    Ajustes na welcome message,  see you spacecowboy

--- telegramapi/telegram_bot.py
+++ telegramapi/telegram_bot.py
@@ -8,11 +8,11 @@ bot = telebot.TeleBot(TOKEN)
 
 standard_rep = '''Olá! Eu sou o bot dos horários de ônibus SIT Macaé (não oficial). 
     
-Por favor, escolha um dos comandos abaixo:
+Por favor, DIGITE um dos comandos abaixo:
 
 - linhas : lista as linhas disponíveis para consulta.
 
-- print [linha] para consultar a tabela de horários desejada. 
+- print [linha] : consultar a tabela de horários desejada. 
 
 (Exemplo: print A33)
 
@@ -26,19 +26,22 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda msg:True)
 def list_lines(message):
-    print(f'{datetime.now()}: user {message.from_user.username} ({message.from_user.first_name}): "{message.text}"')
+    print(f'{datetime.now()}: "{message.text}"')
     print()
-    if('linhas' in message.text.lower()):
+    text = message.text.lower()
+    if('linhas' in text):
         linhas = sitcalc.print_lines_list()
-        bot.reply_to(message, 'Linhas disponíveis para consulta:\n\n'+linhas+'\n(Digite "print XXX" para imprimir a tabela de horários da linha XXX)')
-    elif('print' in message.text.lower()):
+        bot.reply_to(message, 'Linhas disponíveis¹ para consulta:\n\n'+linhas+'\n(Digite "print XXX" para imprimir a tabela de horários da linha XXX)\n\n ¹ Apenas linhas do Terminal Central.')
+    elif('print' in text):
         rep = print_tabela(message)
+        bot.reply_to(message, rep)
+    elif('see you' in text):
+        rep = 'See you space cowboy...'
         bot.reply_to(message, rep)
     else:
         rep = standard_rep
         bot.reply_to(message, rep)
 
-# @bot.message_handler(commands=['print'])
 def print_tabela(message):
     line_code = message.text.split(' ')[1].upper()
     if (line_code in sitcalc.get_lines_list()):
